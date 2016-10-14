86c787dec3a69dae40807a037cb97609862fc1d2
rem <kremartem@gmail.com>
2016-10-14 16:31:10 +0300

    a

--- config/initializers/my_first.rb
+++ config/initializers/my_first.rb
@@ -1,6 +1,6 @@
 
 def self.add_to_const_blorgh
-  Rails.configuration.x.require_gem += {:gem => 'my_first_gem', :title => 'My first gem'}
+  Rails.configuration.x.require_gem << {:gem => 'my_first_gem', :title => 'My first gem'}
 end
 
 if Rails.configuration.x.require_gem.present?
