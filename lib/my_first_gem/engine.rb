947228b8547502c174bd7428894450ee15bab58c
rem <kremartem@gmail.com>
2016-10-14 16:07:59 +0300

    a

--- lib/my_first_gem/engine.rb
+++ lib/my_first_gem/engine.rb
@@ -1,15 +1,6 @@
 module MyFirstGem
   class Engine < ::Rails::Engine
     isolate_namespace MyFirstGem
-    Rails.application.configure do
-      # Settings specified here will take precedence over those in config/application.rb.
-
-      # The test environment is used exclusively to run your application's
-      # test suite. You never need to work with it otherwise. Remember that
-      # your test database is "scratch space" for the test suite and is wiped
-      # and recreated between test runs. Don't rely on the data there!
-      config.my_conf = true
-    end
-
+    Rails.application.config.my_config = 'foo'
   end
 end
